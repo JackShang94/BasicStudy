b1ea30f2a55d3edfa2c45f6a4c3693803303d521
Shang Liangliang <jackshang94@gmail.com>
2019-09-19 11:16:26 +0800

    update

--- DeepDiveIntoPython/Day01/CalBMI.py
+++ DeepDiveIntoPython/Day01/CalBMI.py
@@ -1,9 +1,15 @@
 # Calculate a BMI
 height = float(input('Enter your height in m: '))
-weight = float(input('Enter your weight in m : '))
+weight = float(input('Enter your weight in kg : '))
 
 bmi = weight / (height * height)
 
 print('Your height is ' + str(height) + 'm')
-print('Your weight is ' + str(weight) + 'm')
+print('Your weight is ' + str(weight) + 'kg')
 print('Your BMI is ' + str(bmi))
+
+switch = {
+    "a":lambda x:x*2,
+    "b":lambda x:x*3,
+    "c":lambda x:x**x
+}
---
+++ DeepDiveIntoPython/Day01/Excercises.py
@@ -0,0 +1,13 @@
+# 1)
+a,b,c = 4,2,5
+result1 = a*c / 3
+result2 = b-2
+print(result1 >= result2)
+
+print(8 == 8.0)
+
+print (8 == '8.0')
+
+print(True and False or  False)
+
+print((True or False) and  False)
