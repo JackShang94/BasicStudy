f7fd6196c1cf2724ce9237d79f6b87fd143e43e4
Shang Liangliang <jackshang94@gmail.com>
2019-09-19 14:43:16 +0800

    update

--- DeepDiveIntoPython/Day01/Excercises.py
+++ DeepDiveIntoPython/Day01/Excercises.py
@@ -8,9 +8,28 @@ print(8 == 8.0)
 
 print (8 == '8.0')
 
-print(True and False or  False)
+print(True and False or False)
 
-print((True or False) and  False)
+print((True or False) and False)
 
 print(True and (False or not True))
 
+x,y,z = 1,4,14
+print(x<=1 or y>1 and z<1) # result is true, T or T and F -> T or (T and F) -> T or F -> T
+
+#s1
+time, velocity = 5,3
+temp = time
+time = velocity
+velocity = temp
+print(time,velocity)
+#s2
+time, velocity = 5,3
+time, velocity = velocity,time
+print(time,velocity)
+#s3
+time, velocity = 5,3
+time = time + velocity
+velocity = time - velocity
+time = time - velocity
+print(time,velocity)
