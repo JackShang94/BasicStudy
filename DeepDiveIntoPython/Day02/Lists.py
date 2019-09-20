5a35c327cd3b8db15a6cf27e8de86a17680581a9
Shang Liangliang <jackshang94@gmail.com>
2019-09-20 11:46:50 +0800

    update

---
+++ DeepDiveIntoPython/Day02/Lists.py
@@ -0,0 +1,19 @@
+bankList = ['DBS','UOB','OCBC']
+friendList = ['Peter',['John','Mary'],'David']
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+emptyList = []
+mixedList = ['Peter',100,23.5,[10,20]]
+
+# Activity 2
+markList = [1,2,3,4,5,6,7,8,9,10]
+print(markList[0])
+
+sum = markList[-2] + markList[-1]
+print(sum)
+
+markList[1] = markList[1]*2
+print(markList[1])
+
+letters = ['a','b']
+letters.append('c')
+letters.
