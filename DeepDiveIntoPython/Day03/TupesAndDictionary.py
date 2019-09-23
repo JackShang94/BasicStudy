9b608bc142d2e73d1c40e2573f1f4629b7599fac
Shang Liangliang <jackshang94@gmail.com>
2019-09-23 09:53:33 +0800

    update

---
+++ DeepDiveIntoPython/Day03/TupesAndDictionary.py
@@ -0,0 +1,35 @@
+#Tuple
+tupl1 = (1,2,3)
+tupl1[1]
+
+#Dictionary
+phoneBook = {'Hafiz':'91238980',1:'b'} # key can be any type
+print(phoneBook)
+# add new key value pair
+phoneBook[2] = 'new'
+print(phoneBook)
+# replacing value
+phoneBook[1] = 'a'
+print(phoneBook)
+# removing item
+phoneBook
+phoneBook.pop(1)
+print(phoneBook)
+
+phoneBook.pop(3,'Not found')
+# basic functions
+len(phoneBook)
+print(2 in phoneBook)
+
+phoneBook.get(2)
+
+phoneBook.get(2,'Not find')
+
+# Activity 1
+phoneBook.clear()
+phoneBook = {'Jack':'91238980','Lucy':'12345678','Bob':'87654321','Danny':'22222222'}
+for name in phoneBook :
+    print(name,'->', phoneBook[name]) 
+
+for key,value in phoneBook.items():
+    print(key,value)
