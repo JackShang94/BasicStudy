8c8a0e939d80a8ccc283d9de42a38a624cbb830f
Shang Liangliang <jackshang94@gmail.com>
2019-09-24 09:35:21 +0800

    update

---
+++ DeepDiveIntoPython/Day04/ForLoop.py
@@ -0,0 +1,18 @@
+range(9) # 0~8
+range(2,9) # 2~8
+range(2,10,2) # 2,4,6,8
+range(-2,-10,-2) # -2,-4,-6,-8
+# Activity 1
+measurement = [20.1]
+total = 0
+
+## way1
+for item in measurement:
+    total = total + item
+## way 2
+for i in range(0,len(measurement)):
+    total += measurement[i]
+
+average = total/len(measurement)
+
+# Activity 2
