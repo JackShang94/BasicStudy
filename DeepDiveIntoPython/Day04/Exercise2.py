d18a00fbde72407d9b2e3a36728d874d56f1d9a3
Shang Liangliang <jackshang94@gmail.com>
2019-09-24 11:31:14 +0800

    update

--- DeepDiveIntoPython/Day04/Exercise2.py
+++ DeepDiveIntoPython/Day04/Exercise2.py
@@ -1,1 +1,8 @@
+numToConvert = input('Enter a number to convert: ')
+morseCode = ['-----','.----','..---','...--','....-','.....','-....','--...','---..','----.']
+output = ''
+for i in numToConvert:
+    digit = int(i)
+    output = output + morseCode[digit] +'   '
 
+print('The morse code for {} is {}'.format(numToConvert, output))
