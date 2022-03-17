1c38a79f5ee0be89914329d839e75ffd29aac08f
jackshang <jackshang94@gmail.com>
2022-03-17 11:59:04 +0800

    update

--- DeepDiveIntoPython/Day05/SampleAppication.py
+++ DeepDiveIntoPython/Day05/SampleAppication.py
@@ -12,16 +12,19 @@ def init_product_list(p_list):
     p = Product('1027', 'Samsung Galaxy', 988.00)
     p_list.append(p)
 
+
 def list_products(p_list):
     print('{:<20}{:<20}{:<20}{:<20}'.format('Code', 'Name', 'Price', 'GST'))
     for p in p_list:
         print('{:<20}{:<20}${:<20.2f}${:<20.2f}'.format(p.code, p.name, p.price, p.calculate_gst()))
+
 
 def list_products_below_target(p_list, target):
     print('{:6}{:20}{:8}'.format('Code', 'Name', 'Price'))
     for p in p_list:
         if p.price < target:
             print('{:6}{:20}{:8.2f}'.format(p.code, p.name, p.price))
+
 
 product_list = []
 init_product_list(product_list)
